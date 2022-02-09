eb1171b74c8386f5465273a507079eaa5c123db3
Nikita Gerasimov <tariel-x@ya.ru>
2022-02-09 20:18:43 +0300

    almost parsing arrays with reflection

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -3,7 +3,9 @@ package parser
 import (
 	"bytes"
 	"encoding/binary"
+	"errors"
 	"fmt"
+	"reflect"
 	"strconv"
 )
 
@@ -53,11 +55,55 @@ func GetRequest(b []byte) (int, string, error) {
 	return int(intKey), strconv.Itoa(int(intKey)), nil
 }
 
-func readString(data []byte) string {
-	return string(data)
+type decodeElementFunc func(data []byte) (int, interface{}, error)
+
+func decodeArray(data []byte, target interface{}, decodeElement decodeElementFunc) (int, error) {
+	valuePtr := reflect.ValueOf(target)
+	if reflect.TypeOf(target).Kind() != reflect.Ptr || valuePtr.IsNil() {
+		return 0, errors.New("target must be a non-nil pointer")
+	}
+	if reflect.Indirect(valuePtr).Kind() != reflect.Slice {
+		return 0, errors.New("target must be a pointer to slice")
+	}
+
+	offset := 0
+	var arraySize int32
+	if err := decode(data[offset:offset+4], &arraySize); err != nil {
+		return 0, err
+	}
+	offset = offset + 4
+
+	value := valuePtr.Elem()
+
+	for i := 0; i < int(arraySize); i++ {
+
+		elementOffset, element, err := decodeElement(data[offset:])
+		if err != nil {
+			return 0, err
+		}
+		offset = offset + elementOffset
+		value.Set(reflect.Append(value, reflect.ValueOf(element)))
+	}
+
+	return offset, nil
 }
 
-func read(data []byte, s interface{}) error {
+func decodeString(data []byte) (int, string, error) {
+	var offset int
+	var topicNameSize int16
+
+	if err := decode(data[offset:offset+2], &topicNameSize); err != nil {
+		return 0, "", err
+	}
+	offset = offset + 2
+
+	ret := string(data[offset : offset+int(topicNameSize)])
+
+	offset = offset + int(topicNameSize)
+	return offset, ret, nil
+}
+
+func decode(data []byte, s interface{}) error {
 	buffer := bytes.NewBuffer(data)
 	if err := binary.Read(buffer, binary.BigEndian, s); err != nil {
 		return fmt.Errorf("can not decode data into the provided variable: %w", err)
@@ -86,49 +132,55 @@ type ProduceRequestPartition struct {
 }
 
 type Message struct {
-	Crc        int32
-	MagicByte  int8
-	Attributes int8
-	Timestamp  int64
-	Key        []byte
-	Value      []byte
+	Offset         int64
+	MessageSize    int32
+	LeaderEpoch    int32
+	MagicByte      int8
+	Crc            int32
+	Flags          int16
+	LastOffsetData int32
+	FirstTimestamp int64
+	LastTimestamp  int64
+	ProducerID     int64
+	ProducerEpoch  int16
+	BaseSequence   int32
+	Size           int32
 }
 
 type ProduceRequestParser struct {
 }
 
 func (p *ProduceRequestParser) Parse(data []byte) (*ProduceRequest, error) {
-	offset := 0
+	var offset int
 	reqpart1 := struct {
-		Size               int32
-		ApiKey             int16
-		ApiVersion         int16
-		CorrelationId      int32
-		ClientIDStringSize int16
+		Size          int32
+		ApiKey        int16
+		ApiVersion    int16
+		CorrelationId int32
 	}{}
-	if err := read(data[offset:14], &reqpart1); err != nil {
+	if err := decode(data[offset:12], &reqpart1); err != nil {
 		return nil, err
 	}
-	offset = 14
-	clientID := readString(data[offset : offset+int(reqpart1.ClientIDStringSize)])
-	offset = offset + int(reqpart1.ClientIDStringSize)
+	offset += 12
+	read, clientID, err := decodeString(data[offset:])
+	offset = offset + read
 
 	reqpart2 := struct {
 		TransactionID int16
 		RequiredAcks  int16
 		Timeout       int32
-		TopicsSize    int32
 	}{}
-	if err := read(data[offset:offset+12], &reqpart2); err != nil {
+	if err := decode(data[offset:offset+8], &reqpart2); err != nil {
 		return nil, err
 	}
-	offset = offset + 12
+	offset += 8
 
-	read, topics, err := p.parseTopics(int(reqpart2.TopicsSize), data[offset:])
+	var topics []ProduceRequestTopic
+	read, err = decodeArray(data[offset:], &topics, p.parseTopic)
 	if err != nil {
 		return nil, err
 	}
-	offset = offset + read
+	offset += read
 
 	return &ProduceRequest{
 		ApiKey:        reqpart1.ApiKey,
@@ -141,29 +193,55 @@ func (p *ProduceRequestParser) Parse(data []byte) (*ProduceRequest, error) {
 	}, nil
 }
 
-func (p *ProduceRequestParser) parseTopics(count int, data []byte) (int, []ProduceRequestTopic, error) {
-	offset := 0
-	topics := make([]ProduceRequestTopic, 0, count)
-	for i := 0; i < count; i++ {
-		topicOffset, topic, err := p.parseTopic(data[offset:])
-		if err != nil {
-			return 0, nil, err
-		}
-		offset = offset + topicOffset
-		topics = append(topics, topic)
-	}
-	return offset, topics, nil
-}
-
-func (p *ProduceRequestParser) parseTopic(data []byte) (int, ProduceRequestTopic, error) {
-	var topicNameSize int16
-	offset := 0
-	if err := read(data[offset:offset+2], &topicNameSize); err != nil {
+func (p *ProduceRequestParser) parseTopic(data []byte) (int, interface{}, error) {
+	var offset int
+	read, topicName, err := decodeString(data[offset:])
+	if err != nil {
 		return 0, ProduceRequestTopic{}, err
 	}
-	offset = offset + 2
-	topicName := readString(data[offset : offset+int(topicNameSize)])
+	offset += read
+
+	var partitions []ProduceRequestPartition
+	read, err = decodeArray(data[offset:], &partitions, p.parsePartition)
+	if err != nil {
+		return 0, ProduceRequestTopic{}, err
+	}
+	offset += read
+
 	return offset, ProduceRequestTopic{
-		TopicName: topicName,
+		TopicName:  topicName,
+		Partitions: partitions,
 	}, nil
+}
+
+func (p *ProduceRequestParser) parsePartition(data []byte) (int, interface{}, error) {
+	var offset int
+	var partitionID int32
+	if err := decode(data[offset:offset+4], &partitionID); err != nil {
+		return 0, nil, err
+	}
+	offset = offset + 4
+
+	var messages []Message
+	read, err := decodeArray(data[offset:], &messages, p.parseMessage)
+	if err != nil {
+		return 0, nil, err
+	}
+	offset += read
+
+	return offset, ProduceRequestPartition{
+		Partition:  partitionID,
+		MessageSet: nil,
+	}, nil
+}
+
+func (p *ProduceRequestParser) parseMessage(data []byte) (int, interface{}, error) {
+	var offset int
+	msg := Message{}
+	if err := decode(data[offset:offset+61], &msg); err != nil {
+		return 0, nil, err
+	}
+	offset += 61
+
+	return offset, msg, nil
 }
