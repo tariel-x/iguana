bcd23e346c9efd2788ca4b94be8255ba70a63816
Nikita Gerasimov <tariel-x@ya.ru>
2022-02-11 14:03:40 +0300

    full produce parser

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -90,17 +90,37 @@ func decodeArray(data []byte, target interface{}, decodeElement decodeElementFun
 
 func decodeString(data []byte) (int, string, error) {
 	var offset int
-	var topicNameSize int16
+	var stringLength int16
 
-	if err := decode(data[offset:offset+2], &topicNameSize); err != nil {
+	if err := decode(data[offset:offset+2], &stringLength); err != nil {
 		return 0, "", err
 	}
 	offset = offset + 2
 
-	ret := string(data[offset : offset+int(topicNameSize)])
+	if stringLength == 0 {
+		return 0, "", nil
+	}
 
-	offset = offset + int(topicNameSize)
+	ret := string(data[offset : offset+int(stringLength)])
+
+	offset = offset + int(stringLength)
 	return offset, ret, nil
+}
+
+func decodeData(data []byte) (int, []byte, error) {
+	var offset int
+
+	length, read := binary.Varint(data[offset:])
+	if read == 0 {
+		return 0, nil, errors.New("not varint")
+	}
+	offset += read
+
+	if length <= 0 {
+		return offset, nil, nil
+	}
+
+	return offset + int(length), data[offset : offset+int(length)], nil
 }
 
 func decode(data []byte, s interface{}) error {
@@ -127,24 +147,39 @@ type ProduceRequestTopic struct {
 }
 
 type ProduceRequestPartition struct {
-	Partition  int32
-	MessageSet []Message
+	Partition   int32
+	RecordBatch RecordBatch
 }
 
-type Message struct {
-	Offset         int64
-	MessageSize    int32
-	LeaderEpoch    int32
-	MagicByte      int8
-	Crc            int32
-	Flags          int16
-	LastOffsetData int32
-	FirstTimestamp int64
-	LastTimestamp  int64
-	ProducerID     int64
-	ProducerEpoch  int16
-	BaseSequence   int32
-	Size           int32
+type RecordBatch struct {
+	FirstOffset          int64
+	Length               int32
+	PartitionLeaderEpoch int32
+	Magic                int8
+	CRC                  int32
+	Attributes           int16
+	LastOffsetDelta      int32
+	FirstTimestamp       int64
+	MaxTimestamp         int64
+	ProducerId           int64
+	ProducerEpoch        int16
+	FirstSequence        int32
+	Records              []Record
+}
+
+type Record struct {
+	Length         int64 //varint
+	Attributes     int8
+	TimestampDelta int64 //varint
+	OffsetDelta    int64 //varint
+	Key            []byte
+	Value          []byte
+	Headers        []Header
+}
+
+type Header struct {
+	HeaderKey   string
+	HeaderValue []byte
 }
 
 type ProduceRequestParser struct {
@@ -222,26 +257,145 @@ func (p *ProduceRequestParser) parsePartition(data []byte) (int, interface{}, er
 	}
 	offset = offset + 4
 
-	var messages []Message
-	read, err := decodeArray(data[offset:], &messages, p.parseMessage)
+	read, messages, err := p.parseRecordBatch(data[offset:])
 	if err != nil {
 		return 0, nil, err
 	}
 	offset += read
 
 	return offset, ProduceRequestPartition{
-		Partition:  partitionID,
-		MessageSet: nil,
+		Partition:   partitionID,
+		RecordBatch: messages,
 	}, nil
 }
 
-func (p *ProduceRequestParser) parseMessage(data []byte) (int, interface{}, error) {
+func (p *ProduceRequestParser) parseRecordBatch(data []byte) (int, RecordBatch, error) {
 	var offset int
-	msg := Message{}
-	if err := decode(data[offset:offset+61], &msg); err != nil {
+	var messageSetSize int32
+	if err := decode(data[offset:offset+4], &messageSetSize); err != nil {
+		return 0, RecordBatch{}, err
+	}
+	offset += 4
+
+	reqpart := struct {
+		FirstOffset          int64
+		Length               int32
+		PartitionLeaderEpoch int32
+		Magic                int8
+		CRC                  int32
+		Attributes           int16
+		LastOffsetDelta      int32
+		FirstTimestamp       int64
+		MaxTimestamp         int64
+		ProducerId           int64
+		ProducerEpoch        int16
+		FirstSequence        int32
+	}{}
+
+	if err := decode(data[offset:offset+57], &reqpart); err != nil {
+		return 0, RecordBatch{}, err
+	}
+	offset += 57
+
+	var records []Record
+	read, err := decodeArray(data[offset:], &records, p.parseRecord)
+	if err != nil {
+		return 0, RecordBatch{}, err
+	}
+	offset += read
+
+	return offset, RecordBatch{
+		FirstOffset:          reqpart.FirstOffset,
+		Length:               reqpart.Length,
+		PartitionLeaderEpoch: reqpart.PartitionLeaderEpoch,
+		Magic:                reqpart.Magic,
+		CRC:                  reqpart.CRC,
+		Attributes:           reqpart.Attributes,
+		LastOffsetDelta:      reqpart.LastOffsetDelta,
+		FirstTimestamp:       reqpart.FirstTimestamp,
+		MaxTimestamp:         reqpart.MaxTimestamp,
+		ProducerId:           reqpart.ProducerId,
+		ProducerEpoch:        reqpart.ProducerEpoch,
+		FirstSequence:        reqpart.FirstSequence,
+		Records:              records,
+	}, nil
+}
+
+func (p *ProduceRequestParser) parseRecord(data []byte) (int, interface{}, error) {
+	var offset int
+
+	length, read := binary.Varint(data)
+	if read == 0 {
+		return 0, nil, errors.New("not varint")
+	}
+	offset += read
+
+	var attributes int8
+	err := decode(data[offset:offset+1], &attributes)
+	if err != nil {
+		return 0, nil, fmt.Errorf("not int8: %w", err)
+	}
+	offset += 1
+
+	timestampDelta, read := binary.Varint(data[offset:])
+	if read == 0 {
+		return 0, nil, errors.New("not varint")
+	}
+	offset += read
+
+	offsetDelta, read := binary.Varint(data[offset:])
+	if read == 0 {
+		return 0, nil, errors.New("not varint")
+	}
+	offset += read
+
+	read, key, err := decodeData(data[offset:])
+	if err != nil {
 		return 0, nil, err
 	}
-	offset += 61
+	offset += read
 
-	return offset, msg, nil
+	read, value, err := decodeData(data[offset:])
+	if err != nil {
+		return 0, nil, err
+	}
+	offset += read
+
+	var headers []Header
+	read, err = decodeArray(data[offset:], &headers, p.parseHeader)
+	if err != nil {
+		return 0, nil, err
+	}
+	offset += read
+
+	return offset, Record{
+		Length:         length,
+		Attributes:     attributes,
+		TimestampDelta: timestampDelta,
+		OffsetDelta:    offsetDelta,
+		Key:            key,
+		Value:          value,
+		Headers:        headers,
+	}, nil
+}
+
+func (p *ProduceRequestParser) parseHeader(data []byte) (int, interface{}, error) {
+	var offset int
+
+	read, key, err := decodeData(data[offset:])
+	if err != nil {
+		return 0, nil, err
+	}
+	offset += read
+
+	read, value, err := decodeData(data[offset:])
+	if err != nil {
+		return 0, nil, err
+	}
+	offset += read
+
+	return offset, Header{
+		HeaderKey:   string(key),
+		HeaderValue: value,
+	}, nil
 }
