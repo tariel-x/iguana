aedd21c43c976ef6c0c20dbf1a49d99f5f21f80f
Nikita Gerasimov <tariel-x@ya.ru>
2022-02-11 14:08:23 +0300

    fix tests

--- internal/parser/parser_test.go
+++ internal/parser/parser_test.go
@@ -41,7 +41,41 @@ func TestParseRequest(t *testing.T) {
 		ClientId:      "rdkafka",
 		RequiredAcks:  -1,
 		Timeout:       5000,
-		Topics:        nil,
+		Topics: []ProduceRequestTopic{
+			{
+				TopicName: "test-data",
+				Partitions: []ProduceRequestPartition{
+					{
+						Partition: 0,
+						RecordBatch: RecordBatch{
+							FirstOffset:          0,
+							Length:               57,
+							PartitionLeaderEpoch: 0,
+							Magic:                2,
+							CRC:                  243385418,
+							Attributes:           0,
+							LastOffsetDelta:      0,
+							FirstTimestamp:       1643903619549,
+							MaxTimestamp:         1643903619549,
+							ProducerId:           -1,
+							ProducerEpoch:        -1,
+							FirstSequence:        -1,
+							Records: []Record{
+								{
+									Length:         7,
+									Attributes:     0,
+									TimestampDelta: 0,
+									OffsetDelta:    0,
+									Key:            nil,
+									Value:          []byte("1"),
+									Headers:        nil,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
 	}
 	assert.NotNil(t, req)
 	assert.Equal(t, expected, *req)
